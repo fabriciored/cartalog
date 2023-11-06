b410a35e95fe23215e88d29ea834f017e530b332
Fabrício Santos <fabriciojlsantos@gmail.com>
2023-11-06 16:23:35 -0300

    feat: import automaker usecases and controller in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AutomakerUseCasesModule } from './use-cases/automaker/automaker.use-cases.module';
+import { AutomakerController } from './controllers/automaker/automaker.controller';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [AutomakerUseCasesModule],
+  controllers: [AutomakerController],
   providers: [],
 })
 export class AppModule {}
