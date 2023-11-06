5c8706290b0eda0eba64662419ee0e6efdd4b725
Fabrício Santos <fabriciojlsantos@gmail.com>
2023-11-06 16:22:57 -0300

    feat: automaker controller

---
+++ src/controllers/automaker/automaker.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Param, Post, Body, Put } from '@nestjs/common';
+import {
+  CreateAutomakerDto,
+  UpdateAutomakerDto,
+} from 'src/core/dtos/automaker.dto';
+import { AutomakerUseCases } from 'src/use-cases/automaker/automaker.use-case';
+
+@Controller('api/automakers')
+export class AutomakerController {
+  constructor(private automakerUseCases: AutomakerUseCases) {}
+
+  @Get()
+  async getAllAutomakers() {
+    return await this.automakerUseCases.getAllAutomakers();
+  }
+
+  @Get(':id')
+  async getAutomakerById(@Param('id') id: any) {
+    return await this.automakerUseCases.getAutomakerById(id);
+  }
+
+  @Post('/create')
+  async createAutomaker(@Body() createAutomakerDto: CreateAutomakerDto) {
+    return await this.automakerUseCases.createAutomaker(createAutomakerDto);
+  }
+
+  @Put(':id')
+  async updateAutomaker(
+    @Param('id') id: string,
+    @Body() updateAutomakerDto: UpdateAutomakerDto,
+  ) {
+    return await this.automakerUseCases.updateAutomaker(id, updateAutomakerDto);
+  }
+}
