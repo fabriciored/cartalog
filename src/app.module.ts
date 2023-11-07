110916d88140faafda772190b143a7a3b24e7f92
Fabrício Santos <fabriciojlsantos@gmail.com>
2023-11-07 16:22:21 -0300

    feat: exceptions module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AutomakerUseCasesModule } from './use-cases/automaker/automaker.use-cases.module';
 import { AutomakerController } from './controllers/automaker/automaker.controller';
+import { ExceptionsModule } from './infrastructure/exceptions/exceptions.module';
 
 @Module({
-  imports: [AutomakerUseCasesModule],
+  imports: [AutomakerUseCasesModule, ExceptionsModule],
   controllers: [AutomakerController],
   providers: [],
 })
---
+++ src/infrastructure/exceptions/exceptions.service.ts
@@ -0,0 +1,30 @@
+import {
+  Injectable,
+  BadRequestException,
+  ForbiddenException,
+  InternalServerErrorException,
+  UnauthorizedException,
+  NotFoundException,
+} from '@nestjs/common';
+import {
+  IException,
+  IFormatExceptionMessage,
+} from '../../domain/exceptions/exceptions.interface';
+
+@Injectable()
+export class ExceptionsService implements IException {
+  badRequestException(data: IFormatExceptionMessage): void {
+    throw new BadRequestException(data);
+  }
+  forbiddenException(data?: IFormatExceptionMessage): void {
+    throw new ForbiddenException(data);
+  }
+  UnauthorizedException(data?: IFormatExceptionMessage): void {
+    throw new UnauthorizedException(data);
+  }
+  NotFoundException(data?: IFormatExceptionMessage): void {
+    throw new NotFoundException(data);
+  }
+}
+
+export const Exceptions = new ExceptionsService();
