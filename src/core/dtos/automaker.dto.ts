f2cf109b674e8f126e1457547ab57a74a5a678dc
Fabrício Santos <fabriciojlsantos@gmail.com>
2023-11-07 02:43:40 -0300

    fix: remove logo from DTO

--- src/core/dtos/automaker.dto.ts
+++ src/core/dtos/automaker.dto.ts
@@ -5,10 +5,6 @@ export class CreateAutomakerDto {
   @IsString()
   @IsNotEmpty()
   name: string;
-
-  @IsString()
-  @IsNotEmpty()
-  logo: string;
 }
 
 export class UpdateAutomakerDto extends PartialType(CreateAutomakerDto) {}
