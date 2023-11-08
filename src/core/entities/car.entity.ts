f61c6d91177c1de643665752abd93ab82ef8e563
Fabrício Santos <fabriciojlsantos@gmail.com>
2023-11-08 12:08:26 -0300

    feat: car entity

---
+++ src/core/entities/car.entity.ts
@@ -0,0 +1,13 @@
+import { Automaker } from './automaker.entity';
+
+export class Car {
+  automaker: Automaker;
+  image: string;
+  name: string;
+  model: string;
+  year: number;
+  transmission: string;
+  fuel: string;
+  power: string;
+  torque: string;
+}
